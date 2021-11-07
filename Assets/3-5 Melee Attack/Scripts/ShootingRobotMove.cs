5a814ea6222a6680adf39a964f5b2179025b6be6
D. Suzuki <bboydaisuke@gmail.com>
2021-11-08 02:04:32 +0900

    第3ターム5週目の内容を追加した

---
+++ Assets/3-5 Melee Attack/Scripts/ShootingRobotMove.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class ShootingRobotMove : MonoBehaviour
+{
+    [SerializeField] float _movePower = 5f;
+    Rigidbody _rb = default;
+    Animator _anim = default;
+    Vector3 _dir = default;
+
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+        _anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        _dir = Input.GetAxisRaw("Horizontal") * Vector3.right + Input.GetAxisRaw("Vertical") * Vector3.forward;
+
+    }
+
+    void FixedUpdate()
+    {
+        _rb.AddForce(_dir.normalized * _movePower);
+    }
+
+    void LateUpdate()
+    {
+        if (_dir != Vector3.zero)
+        {
+            this.transform.forward = _dir;
+        }
+
+        Vector3 forward = _rb.velocity;
+        forward.y = 0;
+
+        if (_anim)
+        {
+            _anim.SetFloat("Speed", forward.magnitude);
+        }
+    }
+}
---
+++ Assets/3-5 Melee Attack/Scripts/ShootingRobotShoot.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootingRobotShoot : MonoBehaviour
+{
+    [SerializeField] Transform _muzzle = default;
+    [SerializeField] LineRenderer _line = default;
+    [SerializeField] float _lineLength = 5f;
+    [SerializeField] float _flashInterval = 0.05f;
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            StartCoroutine(ShootRoutine());
+        }
+    }
+
+    IEnumerator ShootRoutine()
+    {
+        _line.SetPosition(0, _muzzle.position);
+        Vector3 lineTarget = _muzzle.position + this.transform.forward * _lineLength;
+        _line.SetPosition(1, lineTarget);
+        yield return new WaitForSeconds(_flashInterval);
+        _line.SetPosition(1, _line.GetPosition(0));
+    }
+}
