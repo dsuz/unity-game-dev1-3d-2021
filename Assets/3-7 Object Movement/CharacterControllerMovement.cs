b0af4688b7fb1e6db958263948946ed17ff83d63
D. Suzuki <bboydaisuke@gmail.com>
2021-11-25 02:45:14 +0900

    第3ターム7週目の内容を追加した

--- Assets/3-7 Object Movement/CharacterControllerMovement.cs
+++ Assets/3-7 Object Movement/CharacterControllerMovement.cs
@@ -1,11 +1,14 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
+/// <summary>
+/// Character Controller コンポーネントを使ってオブジェクトを動かす
+/// </summary>
 [RequireComponent(typeof(CharacterController))]
 public class CharacterControllerMovement : MonoBehaviour
 {
+    /// <summary>動かすのに使う方法</summary>
     [SerializeField] CharacterControllerMoveMethod _moveMethod = CharacterControllerMoveMethod.Move;
+    /// <summary>移動速度</summary>
     [SerializeField] float _speed = 3f;
     CharacterController _controller = default;
 
@@ -16,6 +19,7 @@ public class CharacterControllerMovement : MonoBehaviour
 
     void Update()
     {
+        // 入力を受け取り、カメラを基準にした XZ 平面上に変換する
         Vector3 dir = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
         dir = Camera.main.transform.TransformDirection(dir);
         dir.y = 0;
@@ -43,19 +47,30 @@ public class CharacterControllerMovement : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// Character Controller を使った衝突判定のコールバック関数
+    /// </summary>
+    /// <param name="hit"></param>
     void OnControllerColliderHit(ControllerColliderHit hit)
     {
         Debug.Log($"{hit.collider.name} に衝突した(OnControllerColliderHit)");
     }
 
+    /// <summary>
+    /// Character Controller は Collider ではなく Rigidbody も必要ないので、OnCollisionEnter は呼ばれない
+    /// </summary>
+    /// <param name="collision"></param>
     void OnCollisionEnter(Collision collision)
     {
         Debug.Log($"{collision.gameObject.name} に衝突した(OnCollisionEnter)");
     }
 }
 
+/// <summary>Character Controller を使って動かす時にどの方法を使うか</summary>
 enum CharacterControllerMoveMethod
 {
+    /// <summary>Move メソッドを使う</summary>
     Move,
+    /// <summary>SimpleMove メソッドを使う</summary>
     SimpleMove,
 }
