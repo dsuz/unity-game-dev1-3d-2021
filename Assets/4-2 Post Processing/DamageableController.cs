bf98d7cbd220fa583a063cfa881c843beb8fc551
D. Suzuki <bboydaisuke@gmail.com>
2021-12-09 01:21:57 +0900

    第4ターム2週目の内容を追加した

---
+++ Assets/4-2 Post Processing/DamageableController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// ダメージとライフを制御するコンポーネント
+/// </summary>
+public class DamageableController : MonoBehaviour
+{
+    /// <summary>初期ライフ</summary>
+    [SerializeField, Range(1, 99999)] int m_initialLife = 5000;
+    /// <summary>現在のライフ</summary>
+    int m_life;
+
+    private void Start()
+    {
+        m_life = m_initialLife;
+    }
+
+    /// <summary>
+    /// ダメージを与える
+    /// </summary>
+    /// <param name="damage">ダメージ量</param>
+    public void Damage(int damage)
+    {
+        m_life -= damage;
+        if (m_life < 1)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/4-2 Post Processing/FPSShootController.cs
@@ -0,0 +1,110 @@
+﻿using UnityEngine;
+
+public class FPSShootController : MonoBehaviour
+{
+    /// <summary>FPS のカメラ</summary>
+    [SerializeField] Camera m_mainCamera;
+    /// <summary>照準となる UI オブジェクト</summary>
+    [SerializeField] UnityEngine.UI.Image m_crosshair;
+    /// <summary>照準に敵を捕らえていない時の色</summary>    
+    [SerializeField] Color m_noTarget = Color.white;
+    /// <summary>照準に敵を捕らえている時の色</summary>
+    [SerializeField] Color m_onTarget = Color.red;
+    /// <summary>射撃可能距離</summary>
+    [SerializeField, Range(1, 200)] float m_shootRange = 10f;
+    /// <summary>照準の Ray が当たる Layer</summary>
+    [SerializeField] LayerMask m_shootingLayer;
+    /// <summary>攻撃したらダメージを与えられる対象</summary>
+    DamageableController m_target;
+    /// <summary>攻撃した時に加える力のスカラー量</summary>
+    [SerializeField] float m_shootPower = 50f;
+    /// <summary>射撃音</summary>
+    [SerializeField] AudioClip m_shootingSfx;
+    
+
+    void Start()
+    {
+        // マウスカーソルを消す（実行中は ESC キーを押すとマウスカーソルが表示される）
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+
+        if (!m_mainCamera)
+        {
+            m_mainCamera = Camera.main;
+            if (!m_mainCamera)
+            {
+                Debug.LogError("Main Camera is not found.");
+            }
+        }
+    }
+
+    void Update()
+    {
+        Aim();
+        Shoot();
+    }
+
+    void OnDestroy()
+    {
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+    }
+
+    /// <summary>
+    /// 照準を操作する
+    /// 照準にダメージを与えられる対象がいる場合に照準の色を変え、その対象を m_target に保存する
+    /// </summary>
+    void Aim()
+    {
+        Ray ray = m_mainCamera.ScreenPointToRay(m_crosshair.rectTransform.position);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, m_shootRange, m_shootingLayer))
+        {
+            m_target = hit.collider.GetComponent<DamageableController>();
+
+            if (m_target)
+            {
+                m_crosshair.color = m_onTarget;
+            }
+            else
+            {
+                m_crosshair.color = m_noTarget;
+            }
+        }
+        else
+        {
+            m_target = null;
+            m_crosshair.color = m_noTarget;
+        }
+    }
+
+    /// <summary>
+    /// 敵を撃つ
+    /// </summary>
+    private void Shoot()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            if (m_target)
+            {
+                if (m_shootingSfx)
+                {
+                    AudioSource.PlayClipAtPoint(m_shootingSfx, this.transform.position);
+                }
+
+                m_target.Damage(1);
+
+                Rigidbody rb = m_target.GetComponent<Rigidbody>();
+                if (rb)
+                {
+                    // 斜め上方向に力を加える
+                    Vector3 dir = m_target.transform.position - this.transform.position;
+                    dir.y = 0;
+                    dir = (dir.normalized + Vector3.up).normalized;
+                    rb.AddForce(dir * m_shootPower, ForceMode.Impulse);
+                }
+            }
+        }
+    }
+}
