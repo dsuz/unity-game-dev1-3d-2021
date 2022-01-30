d44705a3bfc2dad05dcdd180863e25bc6a6fc0c2
D. Suzuki <bboydaisuke@gmail.com>
2022-01-31 02:34:40 +0900

    第4ターム5週目の内容を追加した

---
+++ Assets/4-5 Advanced Animation/Blend Tree/WalkForward.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class WalkForward : MonoBehaviour
+{
+    [SerializeField] float _power = 1f;
+    Rigidbody _rb = default;
+    Animator _anim = default;
+
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+        _anim = GetComponent<Animator>();
+    }
+
+    void FixedUpdate()
+    {
+        _rb.AddForce(this.transform.forward * _power, ForceMode.Force);
+    }
+
+    void LateUpdate()
+    {
+        _anim.SetFloat("Speed", _rb.velocity.magnitude);
+    }
+}
